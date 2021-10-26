862cc6b7d06de7f1e0cbf804682815aa49bc3f9c
Сергей Усков <q3mydoom@hotmail.com>
2021-10-26 13:34:07 +0500

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Homework_3._10_Task_1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter number:");
+            int x = int.Parse(Console.ReadLine());
+
+            if (x % 2 == 0)
+            {
+                Console.WriteLine("x = even");
+            } else Console.WriteLine("x = odd");
+
+            Console.ReadKey();
+        }
+    }
+}
